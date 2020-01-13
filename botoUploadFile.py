d436d89798d65b7581c281cc1ffb0a1072b96662
jcp-ITS <jpinasanz@sdsu.edu>
2020-01-13 10:37:12 -0800

    made changes to botoUploadFile.py

--- botoUploadFile.py
+++ botoUploadFile.py
@@ -1,5 +1,6 @@
 import logging
 import boto3
+import sys
 from botocore.exceptions import ClientError
 
 def upload_file(file_name,bucket,object_name=None):
@@ -13,12 +14,20 @@ def upload_file(file_name,bucket,object_name=None):
        #s3.upload_file(file_name,'its-demo-bucket','LectureAudio/'.object_name)
     except ClientError as e:
         logging.error(e)
+        print ('File failed to upload!')
         return False
+    print ('File Uploaded Successfully!')
     return True
 
+a1 = input('Input File Location: ')
+a2 = input('Input Bucket Name: ')
+a3 = input('Input File Name: ')
+
+upload_file(a1,a2,a3)
 
 
-bucketName='its-demo-bucket'
-objectName='transcriptionFile.mp3'
+#bucketName='its-demo-bucket'
+#objectName='transcriptionFile.mp3'
 
-upload_file('/mnt/c/users/jc/desktop/ITS/AWS/Amazon_Transcribe/transcribe-sample.mp3',bucketName,objectName)
+#upload_file(a1,bucketName,a2)
+#upload_file('/mnt/c/users/jc/desktop/ITS/AWS/Amazon_Transcribe/transcribe-sample.mp3',bucketName,objectName)
