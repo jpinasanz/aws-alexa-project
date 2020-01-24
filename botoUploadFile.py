21ab5e1405959d14127d78c36f7b90ec5bbbb20f
jcp-ITS <jpinasanz@sdsu.edu>
2020-01-24 10:45:58 -0800

    added notes to botoUploadfile

--- botoUploadFile.py
+++ botoUploadFile.py
@@ -6,15 +6,22 @@ from botocore.exceptions import ClientError
 import sys
 
 def upload_file(file_name,bucket,object_name=None):
-
+#
+#This pulls the filename from the input file path and concactinates it to the object name.
+#Currently, it only does it when the 3rd argument is 'None'.
+#Need to change this so you can input the file location in the S3 and the script will place
+#the file in the right folder in the S3.
+#
     if object_name is None:
         tail = os.path.split(file_name)
         object_name = object_name + tail[1]
         #object_name = "LectureAudio/Spring2020/" + tail[1]
         print ("file name is: ", object_name)
-
+        
+    #Calls S3 service, sets it to 's3_client'
     s3_client=boto3.client('s3')
 
+    #The actual function call to 'upload_file'
     try:
        response=s3_client.upload_file(file_name,bucket,object_name)
 
@@ -25,11 +32,14 @@ def upload_file(file_name,bucket,object_name=None):
     print ('File Uploaded Successfully!')
     return True
 
+#directory of the local files ~./c/blah/blahblah/audio.mp3
 a1 = input('Input File Location: ')
-a2 = input('Input Bucket Name: ')
-a3 = input('Input Bucket Location: ')
-#a3 = None
-
+#name of the S3 bucket (ie: its-demo-bucket)
+a2 = input('Input Bucket Name: ') 
+#this is normally the object name in the bucket, but we found that if you set this to
+#a directory name, it creates a directory in the S3. This can be used to place the file
+#in a specific location in the S3
+a3 = input('Input Bucket Location: ') 
 
 upload_file(a1,a2,a3)
 
